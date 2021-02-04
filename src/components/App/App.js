e25a055852e48770f60915f3dee499c7ee9016dc
tashiad <tashiadavis10@gmail.com>
2021-02-03 18:41:07 -0700

    Build App component outline

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import './App.css'
 import '../Homepage/Homepage'
-import '../Poster/Poster'
 import movieData from '../movieData'
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: movieData
+    }
+  }
 
+  render() {
+    return(
+      <>
+        <header>
+          <h1>Rancid Tomatillos</h1>
+        </header>
+        <Homepage movies={this.state.movies}/>
+      </>
+    )
+  }
+}
 
-export default App;
+export default App
