1671a77851143bb87a5c51f39b932198fcc5f57f
tashiad <tashiadavis10@gmail.com>
2021-02-03 18:41:20 -0700

    Build Poster component outline

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,5 +1,14 @@
-import React, { Component } from 'react';
-import './Poster.css';
+import React, { Component } from 'react'
+import './Poster.css'
 
+const Poster = ({ id, title, poster_path, average_rating }) => {
+  return (
+    <article>
+      <h2>{title}</h2>
+      <img src={poster_path} alt={`${title} poster`}>
+      <p>{average_rating.toFixed(2)}</p>
+    </article>
+  )
+}
 
-export default Poster;
+export default Poster
