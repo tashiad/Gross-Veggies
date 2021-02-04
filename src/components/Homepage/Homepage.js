b4711e5d569b42bcb781e836d6f071aaf1708afb
tashiad <tashiadavis10@gmail.com>
2021-02-03 18:41:15 -0700

    Build Homepage component outline

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,5 +1,25 @@
-import React, { Component } from 'react';
-import './Homepage.css';
+import React, { Component } from 'react'
+import './Homepage.css'
+import '../Poster/Poster'
 
+const Homepage = ({ movies }) => {
+  const posterCards = movies.map(movie => {
+    return (
+      <Poster
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        poster_path={movie.poster_path}
+        average_rating={movie.average_rating}
+      />
+    )
+  })
 
-export default Homepage;
+  return (
+    <main>
+      {posterCards}
+    </main>
+  )
+}
+
+export default Homepage
