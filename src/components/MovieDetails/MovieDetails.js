0ac21f01fa5177ccde1b7abe912edbca6ac98f9e
tashiad <tashiadavis10@gmail.com>
2021-02-07 11:17:54 -0700

    Style banner image and back button

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,9 +2,8 @@ import React from 'react'
 import './MovieDetails.css'
 
 const MovieDetails = ({currentMovie, clearCurrentMovie}) => {
-  console.log(currentMovie);
   const formattedGenres = currentMovie.genres.map((genre, index) => {
-    return (<span key={index} className="genre">{genre}</span>)
+    return (<span key={index} className="movie-genre">{genre}</span>)
   })
 
   const date = new Date(currentMovie.release_date)
@@ -19,23 +18,23 @@ const MovieDetails = ({currentMovie, clearCurrentMovie}) => {
 
   return (
     <>
-      <section className='banner'>
-        <h2>{currentMovie.title}</h2>
-        <img className='backdrop' src={currentMovie.backdrop_path} alt={`backdrop for ${currentMovie.title}`}/>
+      <section className="movie-banner">
+        <img className="movie-backdrop" src={currentMovie.backdrop_path} alt={`backdrop for ${currentMovie.title}`}/>
+        <h2 className="movie-title">{currentMovie.title}</h2>
       </section>
-      <button className="back-to-main" onClick={clearCurrentMovie}>Back To The Main Page</button>
-      <section className='full-movie-details'>
-        <img className='details-poster'src={currentMovie.poster_path} alt={`poster for ${currentMovie.title}`}/>
-        <div className='movie-info'>
-          {!currentMovie.genres ? null : <p><span className="label">Genre: </span>{formattedGenres}</p>}
+      <button className="back-button" onClick={clearCurrentMovie}>⬅ Back</button>
+      <section className="full-movie-details">
+        <img className="movie-details-poster"src={currentMovie.poster_path} alt={`poster for ${currentMovie.title}`}/>
+        <div className="movie-info">
+          {!currentMovie.genres ? null : <p><span className="movie-details-label">Genre: </span>{formattedGenres}</p>}
           {!currentMovie.average_rating ? null : <p><span className="tomato">🍅</span>{currentMovie.average_rating.toFixed(1)}/10 gross veggies</p>}
-          {!currentMovie.tagline ? null : <p className="tagline">{currentMovie.tagline}</p>}
-          {!currentMovie.release_date ? null : <p><span className="label">Release Date: </span>{formattedDate}</p>}
-          {!currentMovie.runtime ? null : <p><span className="label">Runtime: </span>{formattedRuntime}</p>}
-          {!currentMovie.budget ? null : <p><span className="label">Budget: </span>{currentMovie.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
-          {!currentMovie.revenue ? null : <p><span className="label">Revenue: </span>{currentMovie.revenue.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
+          {!currentMovie.tagline ? null : <p className="movie-tagline">"{currentMovie.tagline}"</p>}
+          {!currentMovie.release_date ? null : <p><span className="movie-details-label">Release Date: </span>{formattedDate}</p>}
+          {!currentMovie.runtime ? null : <p><span className="movie-details-label">Runtime: </span>{formattedRuntime}</p>}
+          {!currentMovie.budget ? null : <p><span className="movie-details-label">Budget: </span>{currentMovie.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
+          {!currentMovie.revenue ? null : <p><span className="movie-details-label">Revenue: </span>{currentMovie.revenue.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
         </div>
-        {!currentMovie.overview ? null : <p className='movie-overview'>{currentMovie.overview}</p>}
+        {!currentMovie.overview ? null : <p className="movie-overview">{currentMovie.overview}</p>}
       </section>
     </>
   )
