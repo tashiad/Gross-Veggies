753568df07a5e375426e003f6c285918e711328c
Max Bregman <bregmanmax91@gmail.com>
2021-02-09 19:26:07 -0700

    got close

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 import Homepage from '../Homepage/Homepage'
 import MovieDetails from '../MovieDetails/MovieDetails'
+import {Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       movies: [],
-      currentMovie: '',
       error: '',
       loading: true
     }
@@ -25,27 +25,26 @@ class App extends Component {
       })
       .then(data => this.setState({
         movies: data.movies,
-        currentMovie: '',
         error: '',
         loading: false
       }))
       .catch(error => this.setState({ error: 'Unable to reach movie database. Please refresh the page or try again later.' }))
   }
 
-  openDetails = (id) => {
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        } else {
-          throw new Error('There be an Ogre')
-        }
-      })
-      .then(data => this.setState({
-        currentMovie: data.movie
-      }))
-      .catch(error => this.setState({ error: 'Unable to find the movie you were looking for. Please try another movie.' }))
-  }
+  // openDetails = (id) => {
+  //   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json()
+  //       } else {
+  //         throw new Error('There be an Ogre')
+  //       }
+  //     })
+  //     .then(data => this.setState({
+  //       currentMovie: data.movie
+  //     }))
+  //     .catch(error => this.setState({ error: 'Unable to find the movie you were looking for. Please try another movie.' }))
+  // }
 
   clearCurrentMovie = () => {
     this.setState({currentMovie: ''})}
@@ -66,19 +65,38 @@ class App extends Component {
             <h2 className="error-message">{this.state.error}</h2>}
         </div>
 
-        {!this.state.currentMovie &&
+              
+        
+        {/* {!this.state.currentMovie &&
           <Homepage
             movies={this.state.movies}
             openDetails ={this.openDetails}
           />
-        }
+        } */}
 
-        {this.state.currentMovie &&
+      <Switch>
+        <Route 
+          path='/movies/:movieId'
+          render={({match}) => {
+
+            const {movieId} = match.params
+
+            console.log(movieId)
+            return (<MovieDetails movieId={movieId}/>)
+          }}
+        />
+        <Route path="/" render={() =>
+          <Homepage
+            movies={this.state.movies}
+          />}
+        />
+      </Switch>
+        {/* {this.state.currentMovie &&
           <MovieDetails
             currentMovie={this.state.currentMovie}
             clearCurrentMovie={this.clearCurrentMovie}
           />
-        }
+        } */}
       </>
     )
   }
--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './MovieDetails.css'
+import { Link } from 'react-router-dom'
 
-const MovieDetails = ({currentMovie, clearCurrentMovie}) => {
+class MovieDetails extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        filmId: parseInt(this.props.movieId),
+        currentMovie: {},
+      }
+    }
+   
+componentWillMount() {
+  console.log('did mount');
+    this.getData()
+    }
+
+async getData() {
+  console.log(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.filmId}`)
+
+  const movie = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
+
+  const parsedMovie = await movie.json()
+
+  this.setState({
+    currentMovie: parsedMovie.mo
+  })
+
+}
+
+
+    // .then(response => {
+    //   if (response.ok) {
+    //     return response.json()
+    //   } else {
+    //     throw new Error('There be an Ogre')
+    //   }
+    // })
+    // .then(data => console.log(data.movie))
+    // .then(data => this.setState({
+    //   currentMovie: data.movie,
+    //   loading: false
+    // }))
+
+    // .catch(error => this.setState({ error: 'Unable to find the movie you were looking for. Please try another movie.' }))
+    // }
+
+  render() {
+
+
+  
+
+  const currentMovie = this.state.currentMovie
+
+  console.log('should be state',this.state)
+
   const formattedGenres = currentMovie.genres.map((genre, index) => {
     return (<span key={index} className="movie-genre">{genre}</span>)
   })
@@ -14,37 +67,48 @@ const MovieDetails = ({currentMovie, clearCurrentMovie}) => {
   const runtimeMinutes = currentMovie.runtime % 60
   const formattedRuntime = runtimeHours === 1 ?
     `${runtimeHours} hour, ${runtimeMinutes} mins` :
-    `${runtimeHours} hours, ${runtimeMinutes} mins`
+    `${runtimeHours} hours, ${runtimeMinutes} mins`;
 
-  return (
-    <>
-      <section className="movie-banner">
-        <img className="movie-backdrop" src={currentMovie.backdrop_path} alt={`backdrop for ${currentMovie.title}`}/>
-        <h2 className="movie-title">{currentMovie.title}</h2>
-      </section>
-      <button className="back-button" onClick={clearCurrentMovie}>⬅ Back</button>
-      <section className="full-movie-details">
-        <div className="movie-info-container">
-          <img className="movie-details-poster"src={currentMovie.poster_path} alt={`poster for ${currentMovie.title}`}/>
-          <div className="movie-info">
-            {!currentMovie.tagline ? null : <p className="movie-tagline">"{currentMovie.tagline}"</p>}
-            {!currentMovie.average_rating ? null : <p className="movie-rating"><span className="tomato">🍅</span>{currentMovie.average_rating.toFixed(1)} / 10 gross veggies</p>}
-            {!currentMovie.genres ? null : <div className="movie-genres-container">{formattedGenres}</div>}
-            {!currentMovie.release_date ? null : <p><span className="movie-details-label">Release Date: </span>{formattedDate}</p>}
-            {!currentMovie.runtime ? null : <p><span className="movie-details-label">Runtime: </span>{formattedRuntime}</p>}
-            {!currentMovie.budget ? null : <p><span className="movie-details-label">Budget: </span>{currentMovie.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
-            {!currentMovie.revenue ? null : <p><span className="movie-details-label">Revenue: </span>{currentMovie.revenue.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
+
+
+    return (
+      
+     <>
+        {this.state.currentMovie && 
+        <>
+        <section className="movie-banner">
+          <img className="movie-backdrop" src={currentMovie.backdrop_path} alt={`backdrop for ${currentMovie.title}`}/>
+          <h2 className="movie-title">{currentMovie.title}</h2>
+        </section>
+        <Link to='/'> <button className="back-button">⬅ Back</button></Link>
+        <section className="full-movie-details">
+          <div className="movie-info-container">
+            <img className="movie-details-poster"src={currentMovie.poster_path} alt={`poster for ${currentMovie.title}`}/>
+            <div className="movie-info">
+              {!currentMovie.tagline ? null : <p className="movie-tagline">"{currentMovie.tagline}"</p>}
+              {!currentMovie.average_rating ? null : <p className="movie-rating"><span className="tomato">🍅</span>{currentMovie.average_rating.toFixed(1)} / 10 gross veggies</p>}
+              {!currentMovie.genres ? null : <div className="movie-genres-container">{formattedGenres}</div>}
+              {!currentMovie.release_date ? null : <p><span className="movie-details-label">Release Date: </span>{formattedDate}</p>}
+              {!currentMovie.runtime ? null : <p><span className="movie-details-label">Runtime: </span>{formattedRuntime}</p>}
+              {!currentMovie.budget ? null : <p><span className="movie-details-label">Budget: </span>{currentMovie.budget.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
+              {!currentMovie.revenue ? null : <p><span className="movie-details-label">Revenue: </span>{currentMovie.revenue.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>}
+            </div>
           </div>
-        </div>
-        {!currentMovie.overview ? null :
-          <div className="movie-overview">
-            <h3 className="overview-label">Overview</h3>
-            <p className="overview-text">{currentMovie.overview}</p>
-          </div>
-        }
-      </section>
-    </>
-  )
+        </section>
+        </>}
+      </>
+    )
+  } 
 }
 
 export default MovieDetails
+
+
+
+
+// {!currentMovie.overview ? null :
+//   <div className="movie-overview">
+//     <h3 className="overview-label">Overview</h3>
+//     <p className="overview-text">{currentMovie.overview}</p>
+//   </div>
+// }
