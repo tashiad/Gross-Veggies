275aff3bc37e363b834680780e0686a9b4e18f59
tashiad <tashiadavis10@gmail.com>
2021-02-10 08:08:16 -0700

    Convert Poster to Router Link

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Poster.css'
 import { Link } from 'react-router-dom'
 
-const Poster = ({ id, title, poster_path, average_rating, openDetails }) => {
+const Poster = ({ id, title, poster_path, average_rating }) => {
   return (
-    <Link to={`/movies/${id}`}>
-      <article className="poster" onClick={() => openDetails(id)}>
+    <Link to={`/${id}`} style={{ textDecoration: 'none' }}>
+      <article className="poster">
         <h2 className="poster-title">{title}</h2>
         <img src={poster_path} alt={`${title} poster`} className="poster-img"/>
         <p className="poster-rating">{`🍅 \u00A0
