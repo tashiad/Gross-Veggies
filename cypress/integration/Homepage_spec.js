5d6df0a08ff8b57f442714b1e596eac6828adf87
Max Bregman <bregmanmax91@gmail.com>
2021-02-10 20:03:05 -0700

    make poseter tests

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -10,4 +10,19 @@ describe('Homepage', () => {
     .get('header').contains('h2', 'Movie ratings and more.')
   })
 
+  it('Should show movie data on page load', () => {
+    cy.fixture('mock-movie-data.json')
+    .then((response) => {
+      cy.intercept({
+        method: 'GET',
+        url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      }, {
+        statusCode: 200,
+        body: response
+      })
+    })
+    .get('.poster-title').contains('Money Plane')
+    .get('.poster-img').should('have.attr', 'src').should('include', "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
+    .get('.poster-rating').contains('6.1/10')
+  })
 })
