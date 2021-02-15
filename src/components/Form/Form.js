362bd7c6ac6205b48a4f6129b313049e458e4a37
Max Bregman <bregmanmax91@gmail.com>
2021-02-15 12:05:32 -0700

    cna now update stae of Form while typing

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import  React, { Component } from 'react'
 import './Form.css'
 
-const Form = () => {
-  return (
-    <form>
-      <label>
-        Search:
-        <input type="text" name="name"/>
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  )
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search:
+          <input type="text" name="name" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <button>RT</button>
+      </form>
+    )
+  }
 }
 
 export default Form
